cd607e037c56008423ffc01552b7f613b8d2ccf9
hashland <pauldwamena10@gmail.com>
2019-04-07 12:05:43 -0700

    fix user exist problem

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -3,6 +3,7 @@ var bcrypt = require('bcryptjs');
 var secret=require("../config/secret");
 const randomstring =require("randomstring");
 import {firebase} from "../firebase/firebasekey";
+import { isUndefined } from 'util';
 
 var generateString=function(){
     var theString=randomstring.generate({
@@ -60,55 +61,68 @@ const register=function login(data,callback){
 
 }
 
-       
+const checkifUserExist=function(email,callback){
+    
+    const user=firebase.database().ref("staff").orderByChild('email').equalTo(email);
+    user.on("value",function(snapshot){
+        if (snapshot.exists()) {
+          var  userExist=true
+        }
+        else{
+           var  userExist=false
+        }
+           return callback({
+               userExist:userExist
+           })
+        })
+    } 
 
+       
 
 
 const login=function(data,callback){
-
-    const user=firebase.database().ref("staff").orderByChild('email').equalTo(data.email);
-    user.once("value",function(snapshot){
-        
+   checkifUserExist(data.email,function(detail){
+    var userExist=detail.userExist
+    if(!userExist){
+        return callback({
+            auth:false,
+            response:"User with this email does not exist"
+        })
        
-        if(snapshot.exists()){
-            var user=snapshot.val();
-           
-        var userpassword=user.password;
-        
-        var passwordIsValid = bcrypt.compareSync(data.password, userpassword);
-        
-        if (!passwordIsValid) {
-           var response="The password you entered might be incorrect"
-           var auth=false
-        }else
-        {   
-            var auth=true;
-            var response="Login successfully";
-            var token = jwt.sign({user:user},secret.jwt, {
-                expiresIn: 86400
-              });
-        }
-          
-          
-
-       }else{
-           
-        console.log("hello")  
-        var response="User with this email does not exist"
-        var auth=false
-       }
-           return callback({
-               response:response,
-               auth:auth,
-               token:token
-           })
-        }
-        )
-    }
-
+    }else{
+        const user=firebase.database().ref("staff").orderByChild('email').equalTo(data.email);
+        user.on("child_added",function(snapshot){
+            var userpassword = snapshot.val().password; 
+            var passwordIsValid = bcrypt.compareSync(data.password, userpassword);
             
+            if (!passwordIsValid) {
+               var response="The password you entered might be incorrect"
+               var auth=false
+            }else
+            {   
+                 var auth=true;
+                var response="Login successfully";
+                var token = jwt.sign({user:user},secret.jwt, {
+                    expiresIn: 86400
+                  });
+            }
+              return callback({
+                  auth:auth,
+                  response:response,
+                  token:token
+              })
+                  }
+   
+
+
+
+        )}
+
+                })
+
+
         
-    
+            }
 
 
   
